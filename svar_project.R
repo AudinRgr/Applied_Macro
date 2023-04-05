81283bc7f01926f4e66f7697722d3c20a9c696bd
AudinRgr <106442464+AudinRgr@users.noreply.github.com>
2023-04-05 10:42:13 +0200

    Add files via upload

--- svar_project.R
+++ svar_project.R
@@ -18,6 +18,10 @@ library(ggplot2)
 library(tframePlus)
 library(rio)
 library(tseries)
+library(ggplot2)
+library(dplyr)
+library(car)
+
 
 # Importing data
 setwd("C:/Users/ce pc/Dropbox/Mon Drive/Fac/S2/Applied macroeconometrics/Projet/Data")
@@ -25,6 +29,7 @@ data <- read_excel("données.xlsx")
 data_GDP <- read_excel("données.xlsx", sheet = "GDP_QUARTERLY")
 data <- data.frame(data)
 data_GDP <- data.frame(data_GDP)
+
 
 # Plotting  monthly time series
 ts_brent <- ggplot(data,aes(x=observation_date, y=BRENT) ) + geom_line(color = "#00AFBB", size = 1)
@@ -65,13 +70,15 @@ ts_HICP_overall <- as.quarterly(ts_HICP_overall, FUN=mean, na.rm=TRUE)
 ts_HICP_energy <- ts(data$HICP_ENERGY, start = c(1999,3), end=c(2021,12), frequency=12)
 ts_HICP_energy <- as.quarterly(ts_HICP_energy, FUN=mean, na.rm=TRUE)
 
-ts_EURIBOR_3M <- ts(data$EURIBOR_3M, start = c(1999,3), end=c(2021,11), frequency=12)
+ts_EURIBOR_3M <- ts(data$EURIBOR_3M, start = c(1999,3), end=c(2021,12), frequency=12)
 ts_EURIBOR_3M <- as.quarterly(ts_EURIBOR_3M, FUN=mean, na.rm=TRUE)
 
-ts_GDP <- ts(data_GDP, start = c(1999,10), end=c(2021,12), frequency=4)
-ts_GDP <- ts_GDP[,2]
-#ts_GDP_quart <- as.quarterly(ts_GDP_quart, FUN=mean, na.rm=TRUE)
 
+ts_GDP <- data_GDP[1:91, 2]
+ts_GDP <- ts(ts_GDP, start = c(1999,3), end=c(2021,12), frequency=12)
+ts_GDP <- as.quarterly(ts_GDP, FUN=mean, na.rm=TRUE)
+
+# Stationarisation   
 # ADF test
 adf.test(ts_brent) # Not-stationary
 adf.test(ts_unemployment) # Not-stationary
@@ -81,7 +88,7 @@ adf.test(ts_HICP_energy)  # Not-stationary
 adf.test(ts_EURIBOR_3M)  # Stationary
 adf.test(ts_GDP) # Not-stationary
 
-
+# Stationarization by differenciation
 ts_brent_d=diff(ts_brent,lag=1,differences=1)
 ts_unemployment_d=diff(ts_unemployment,lag=1,differences=1)
 ts_Core_HICP_d=diff(ts_Core_HICP,lag=1,differences=1)
@@ -91,11 +98,10 @@ ts_GDP_d=diff(ts_GDP,lag=1,differences=1)
 ts_EURIBOR_3M_d=diff(ts_EURIBOR_3M, lag=1,differences=1)
 
 
-
-
-
 # Setting restriction matrix:
-amat <- diag(7) # Identical matrix 4X4
+amat <- diag(7) # Identical matrix 7X7
+
+# 1st column
 amat[2,1] <- NA
 amat[3,1] <- NA
 amat[3,2] <- NA
@@ -104,130 +110,130 @@ amat[5,1] <- NA
 amat[6,1] <- NA
 amat[7,1] <- NA
 
+# 2nd column
 amat[3,2] <- NA
 amat[4,2] <- NA
 amat[5,2] <- NA
 amat[6,2] <- NA
 amat[7,2] <- NA
 
+# 3rd column
 amat[4,3] <- NA
 amat[5,3] <- NA
 amat[6,3] <- NA
 amat[7,3] <- NA
 
+# 4th column
 amat[5,4] <- NA
 amat[6,4] <- NA
 amat[7,4] <- NA
 
-amat[6,4] <- NA
-
+# 5th column
 amat[6,5] <- NA
 amat[7,5] <- NA
 
-amat[6,7] <- NA
+# 6th column
+amat[7,6] <- NA
 
-# retaking EURIBOR 3M + refaire tourner => pb GDP
-svar <- cbind(ts_brent_d,ts_HICP_energy_d,ts_HICP_overall_d, ts_Core_HICP_d, ts_unemployment_d, ts_EURIBOR_3M)
-length
-colnames(svar) <- cbind("BRENT", "HICP_energy", "HICP_overall", "HICP_Core", "Unemployment", "GDP", "Euribor3M")
+svar <- cbind(ts_brent_d,ts_HICP_energy_d,ts_HICP_overall_d, ts_Core_HICP_d, ts_unemployment_d, ts_GDP_d, ts_EURIBOR_3M)
 svar
-
-svar <- na.omit(svar) # à régler car missing comment justifier ???
+colnames(svar) <- cbind("Brent", "HICP_energy", "HICP_overall", "HICP_Core", "Unemployment", "GDP","EURIBOR_3M")
+svar <- na.omit(svar)
 svar
 
 # Determining lag ordering
 lagselect <- VARselect(svar, lag.max = 8, type = "both")
-
 lagselect$selection 
-#AIC(n)  HQ(n)  SC(n) FPE(n) 
-# 8      5      2      7
-# => 2 lags
-
-# Optimal = 8 lags 
 lagselect$criteria
+# => AIC criteria => 8 lags
 
-
-model1 <- VAR(svar, p = 8, season = NULL, exog = NULL, type = "const")
-
-SVARmod1 <- SVAR(model1, Amat = amat, Bmat = NULL, hessian = TRUE, estmethod = c("scoring", "direct"))
-
+# Estimating the SVAR
+model1 <- VAR(data.frame(svar), p = 8,  season = NULL, exog = NULL, type = "const")
+SVARmod1 <- SVAR(model1, max.iter = 200, Amat = amat, Bmat = NULL, hessian = TRUE, estmethod = c("direct"))
 SVARmod1
 
-SVARog <- irf(SVARmod1, impulse = "BRENT", response = "BRENT")
-plot(SVARog) # model works => just plot correctly
-SVARog 
-plotIrf(SVAR, name = NULL, ylab = NULL, alpha = 0.3, n.ahead = NULL,
-      filename = NULL, width = 10, height = 6, ...)
-
-SVARHICP <- irf(SVARmod1, impulse = "BRENT", response = "HICP")
-plot(SVARHICP)
-
-SVARCore <- irf(SVARmod1, impulse = "HICP", response = "Unemployment")
-plot(SVARCore)
-
-SVAR_test <- irf(SVARmod1, impulse="BRENT", response = "Unemployment")
-plot(SVAR_test)
-
-SVAR_test2 <- irf(SVARmod1, impulse="BRENT", response = "GDP")
-plot(SVAR_test2)
-
-SVAR_test3 <- irf(SVARmod1, impulse = "HICP", respon)
-
-#IFR
 
 
+# Impulse Response Function 
+# Brent on all
+SVAR_brent_HICP_energy <- irf(SVARmod1, impulse = "Brent", response = "HICP_energy",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_brent_HICP_energy) 
+
+SVAR_brent_HICP_overall <- irf(SVARmod1, impulse = "Brent", response = "HICP_overall",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_brent_HICP_overall)
+
+SVAR_brent_HICP_Core <- irf(SVARmod1, impulse = "Brent", response = "HICP_Core",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_brent_HICP_Core)
+
+SVAR_brent_Un <- irf(SVARmod1, impulse = "Brent", response = "Unemployment",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_brent_Un)
+
+SVAR_brent_GDP <- irf(SVARmod1, impulse = "Brent", response = "GDP",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_brent_GDP)
+
+SVAR_brent_EURIBOR_3M <- irf(SVARmod1, impulse = "Brent", response = "EURIBOR_3M",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_brent_EURIBOR_3M)
+
+# HICP Energy on all
+SVAR_HICP_energy_HICP_overall <- irf(SVARmod1, impulse ="HICP_energy", response ="HICP_overall",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_energy_HICP_overall)
+
+SVAR_HICP_energy_HICP_Core <- irf(SVARmod1, impulse ="HICP_energy", response ="HICP_Core",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_energy_HICP_Core)
+
+SVAR_HICP_energy_Unemployment <- irf(SVARmod1, impulse ="HICP_energy", response ="Unemployment",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_energy_Unemployment)
+
+SVAR_HICP_energy_GDP <- irf(SVARmod1, impulse ="HICP_energy", response ="GDP",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_energy_GDP)
+
+SVAR_HICP_energy_EURIBOR_3M <- irf(SVARmod1, impulse ="HICP_energy", response ="EURIBOR_3M",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_energy_EURIBOR_3M)
+
+# HICP Overall on all
+SVAR_HICP_overall_HICP_Core <- irf(SVARmod1, impulse ="HICP_overall", response ="HICP_Core",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_overall_HICP_Core)
+
+SVAR_HICP_overall_Unemployment <- irf(SVARmod1, impulse ="HICP_overall", response ="Unemployment",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_overall_Unemployment)
+
+SVAR_HICP_overall_GDP <- irf(SVARmod1, impulse ="HICP_overall", response ="GDP",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_overall_GDP)
+
+SVAR_HICP_overall_EURIBOR_3M <- irf(SVARmod1, impulse ="HICP_overall", response ="EURIBOR_3M",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_overall_EURIBOR_3M)
+
+# HICP Core on all
+SVAR_HICP_Core_Unemployment <- irf(SVARmod1, impulse ="HICP_Core", response ="Unemployment",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_Core_Unemployment)
+
+SVAR_HICP_Core_GDP <- irf(SVARmod1, impulse ="HICP_Core", response ="Unemployment",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_Core_GDP)
+
+SVAR_HICP_Core_EURIBOR_3M <- irf(SVARmod1, impulse ="HICP_Core", response ="EURIBOR_3M",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_HICP_Core_EURIBOR_3M)
+
+# Unemployment on all
+SVAR_Unemployment_GDP <- irf(SVARmod1, impulse ="Unemployment", response ="GDP",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_Unemployment_GDP)
+
+SVAR_Unemployment_EURIBOR_3M <- irf(SVARmod1, impulse ="Unemployment", response ="EURIBOR_3M",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_Unemployment_EURIBOR_3M)
+
+# GDP on all
+SVAR_GDP_EURIBOR_3M <- irf(SVARmod1, impulse ="GDP", response ="EURIBOR_3M",  n.ahead = 40, width = 10, height = 6)
+plot(SVAR_GDP_EURIBOR_3M)
+
+# Forecast error variance decomposition
+SVARfevd <- fevd(SVARmod1, n.ahead = 10)
+SVARfevd
+plot(SVARfevd)
 
 
 
-
-
-
-# Time series
-#y <- ts(macro_data$`Output Gap`, start = c(2000,1,1), frequency = 4)
-#pi <- ts(macro_data$CPI, start = c(2000,1,1), frequency = 4)
-#r <- ts(macro_data$RRP, start = c(2000,1,1), frequency = 4)
-
-# Plotting the series
-#ts_plot(y, title = "Output Gap", Xtitle = "Time", Ytitle = "Output Gap")
-
-#ts_plot(pi, title = "Inflation Rate", Xtitle = "Time", Ytitle = "Inflation Rate")
-
-#ts_plot(r, title = "Overnight Reverse Repurchase Rate", Xtitle = "Time", Ytitle = "RRP")
-
-# Setting restriction matrix:
-#amat <- diag(3) # Identical matrix 3X3
-#amat[2,1] <- NA
-#amat[3,1] <- NA
-#amat[3,2] <- NA
-#amat
-# Creation of matrix with NA to fill in
-
-#svar <- cbind(y, pi , r)
-#colnames(svar) <- cbind("Output Gap", "Inflation", "RRP")
-
-# Determining lag ordering
-#lagselect <- VARselect(svar, lag.max = 8, type = "both")
-#lagselect$selection # Optimal = 5 lags 
-#lagselect$criteria
-
-#model1 <- VAR(svar, p = 5, season = NULL, exog = NULL, type = "const")
-
-#SVARmod1 <- SVAR(model1, Amat = amat, Bmat = NULL, hessian = TRUE, estmethod = c("scoring", "direct"))
-
-#SVARmod1
-
-
-# IRF does not work
-
-
-
-
-#SVARfevd <- fevd(SVARmod1, n.ahead = 10)
-#SVARfevd
-#plot(SVARfevd)
-# can decompose effect of shock
   
-  
+
+
   
   
   
